1545fa7d28fb42becbd65effcca81edd7b1543bf
Jonathan Flores <jonathanflores94@hotmail.com>
2020-12-03 17:14:53 -0700

    Add One project with React and AWS

---
+++ Web Applications/React-Meme-Generator-master/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header.js';
+import MemeGenerator from './MemeGenerator.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MemeGenerator />
+    </div>
+  );
+}
+
+export default App;
