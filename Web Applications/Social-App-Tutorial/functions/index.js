987275902011b9ac274b6e81caec3376e9adca3e
Jonathan Flores <Jonathanflores94@hotmail.com>
2021-02-05 10:03:53 -0700

    Working on the social app again

--- Web Applications/Social-App-Tutorial/functions/index.js
+++ Web Applications/Social-App-Tutorial/functions/index.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 
 admin.initializeApp();
@@ -6,19 +6,18 @@ admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
- exports.helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
- });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
 
- exports.getPosts = functions.https.onRequest((req, res) => {
-    admin.firestore().collection('posts').get()
-        .then(data => {
-            let posts = [];
-            data.forEach(doc => {
-                posts.push(doc.data());
-            });
-            return res.json(posts);
-        })
-        .catch(err => console.error(err));
- })
+exports.getPosts = functions.https.onRequest((req, res) => {
+    admin.firestore().collection('posts').get().then((data) => {
+        let posts = [];
+        data.forEach((doc) => {
+            posts.push(doc.data());
+        });
+        return res.json(posts);
+    })
+    .catch((err) => console.error(err));
+});
